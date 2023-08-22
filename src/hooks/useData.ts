164f756780ced8d74af420bb7dab78f76da2e11f
Subu <subutheboss.mani@gmail.com>
2023-08-22 23:31:49 +0530

    test commit

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -16,7 +16,7 @@ const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?:
     useEffect(() => {
       const controller = new AbortController();
   
-      setLoading(true);
+      setLoading(true); 
       apiClient
         .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
         .then((res) => {
